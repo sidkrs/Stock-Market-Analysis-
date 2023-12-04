136d1826e65ea3562aa96b196e3eaeae744afffd
Siddarth Kerkar <106147814+sidkrs@users.noreply.github.com>
2023-12-03 20:25:16 -0500

    Update stock_pred.py

--- stock_pred.py
+++ stock_pred.py
@@ -4,6 +4,11 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 
 def get_stock():
+    '''
+    Parameters: None
+    Returns: a dataframe of stock data
+    Does: Prompts the user to enter a ticker and returns a dataframe of the stock's data.
+    '''
     ticker = input("Enter a ticker: ").upper()
     ticker_data = yf.Ticker(ticker)
     # Fetch data from the maximum available history
@@ -16,9 +21,15 @@ def get_stock():
     # Prepare DataFrame for Prophet
     df = ticker_df[['Date', 'Close']]
     df.columns = ['ds', 'y']
-    return df
+    return df, ticker
 
-def pred(df):
+def pred(df, ticker):
+    '''
+    Parameters: a dataframe of stock data
+    Returns: None
+    Does: Takes a dataframe of stock data and plots a forecast of the stock's price.
+    '''
+    # Fit the model
     prophet = Prophet(daily_seasonality=True)
     prophet.fit(df)
     future = prophet.make_future_dataframe(periods=365)
@@ -28,9 +39,9 @@ def pred(df):
     fig = plot_plotly(prophet, predictions)
     # Update axis titles and add a plot title
     fig.update_layout(
-        xaxis_title="Year",
-        yaxis_title="Price (USD)",
-        title="Stock Price Forecast"
+    xaxis_title="Year",
+    yaxis_title="Price (USD)",
+    title=f"Stock Price Forecast for {ticker}"
     )
     fig.show()
     fig_components = prophet.plot_components(predictions)
@@ -39,8 +50,8 @@ def pred(df):
 
 
 def main():
-    df = get_stock()
-    pred(df)
+    df, ticker = get_stock()
+    pred(df, ticker)
 
 if __name__ == "__main__":
     main()
